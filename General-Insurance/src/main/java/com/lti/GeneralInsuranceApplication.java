6e54be4287faa77b847aff7431225dd843514798
Rituraj <riturajgujare01@gmail.com>
2021-04-17 11:13:20 +0530

    initial commit

---
+++ General-Insurance/src/main/java/com/lti/GeneralInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.lti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralInsuranceApplication.class, args);
+	}
+
+}
