ccf93a39f3d0f356d18e637a272785323e35e569
Abhiruchi <shendkarabhiruchi@gmail.com>
2021-04-18 16:25:00 +0530

    added annotations in depriciation service and repo class

--- General-Insurance/src/main/java/com/lti/repository/DepreciationRepository.java
+++ General-Insurance/src/main/java/com/lti/repository/DepreciationRepository.java
@@ -1,12 +1,15 @@
 package com.lti.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class DepreciationRepository extends GenericRepository {
 
 	public int getVhehiclePrice(int id) {
 		return (Integer)
 				entityManager
-				.createQuery("select v.price from Vehicle v where v.id= : id ")
-				.setParameter("id", id)
+				.createQuery("select v.price from Vehicle v where v.id = :vid ")
+				.setParameter("vid", id)
 				.getSingleResult();
 	}
 }
--- General-Insurance/src/main/java/com/lti/service/DepreciationService.java
+++ General-Insurance/src/main/java/com/lti/service/DepreciationService.java
@@ -1,11 +1,19 @@
 package com.lti.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.lti.repository.DepreciationRepository;
 
+@Service
+@Transactional
 public class DepreciationService {
+	
+	@Autowired
+	private DepreciationRepository depriciationRepo;
 
 	public int getPriceOfVehilce(int id) {
-		DepreciationRepository repo= new DepreciationRepository();
-		return repo.getVhehiclePrice(id);
+		return depriciationRepo.getVhehiclePrice(id);
 	}
 }
