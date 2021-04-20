e4880ca1310bd10277953e28ce8b1c8f0ddeb968
Rituraj <riturajgujare01@gmail.com>
2021-04-20 15:49:24 +0530

    test cases tried

--- General-Insurance/src/test/java/com/lti/GeneralInsuranceApplicationTests.java
+++ General-Insurance/src/test/java/com/lti/GeneralInsuranceApplicationTests.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.lti.entity1.Customer;
+import com.lti.entity1.Depreciation;
+import com.lti.entity1.Policy;
 import com.lti.entity1.Vehicle;
 import com.lti.repository.CustomerRepository;
 import com.lti.repository.DepreciationRepository;
@@ -43,10 +45,24 @@ class GeneralInsuranceApplicationTests {
 	
 	
 	@Test
-	public void getCustomer() {
-		CustomerRepository repo= new CustomerRepository();
-		int sc = repo.fetchByEmailAndPassword("anand@gmail.com", "123");
-		System.out.println(sc);
+	public void addPolicyToVehicle() {
+	GenericRepository dao= new GenericRepository();
+	Customer cust= (Customer)dao.find(Customer.class, 1010);
+	Vehicle vehicle= (Vehicle)dao.find(Vehicle.class, 2031);
+	Depreciation depreciation= (Depreciation)dao.find(Depreciation.class, 5011);
+
+	Policy policy= new Policy();
+	policy.setType("Third-Party");;
+	policy.setRenewal('n');
+	policy.setPolicyStartDate(LocalDate.of(2020, 7, 4));
+	policy.setPolicyEndDate(LocalDate.of(2021, 7, 4));
+	policy.setPremium(30000);;
+	policy.setIdv(900000);
+	policy.setCustomer(cust);
+	policy.setVehicle(vehicle);
+	policy.setDepreciation(depreciation);
+
+	dao.save(policy);
 	}
 
 	
