97c607d50e801d4d4930172e467fc09adde20393
harshit <harshitsingh0608@gmail.com>
2021-04-20 15:48:10 +0530

    controllerclass created in policy

--- General-Insurance/src/test/java/com/lti/GeneralInsuranceApplicationTests.java
+++ General-Insurance/src/test/java/com/lti/GeneralInsuranceApplicationTests.java
@@ -3,11 +3,17 @@ package com.lti;
 import java.time.LocalDate;
 import java.time.Period;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 import com.lti.entity1.Customer;
+import com.lti.entity1.Depreciation;
+import com.lti.entity1.Policy;
 import com.lti.entity1.Vehicle;
 import com.lti.repository.CustomerRepository;
 import com.lti.repository.DepreciationRepository;
@@ -17,30 +23,6 @@ import com.lti.service.DepreciationService;
 @SpringBootTest
 class GeneralInsuranceApplicationTests {
 	
-	@Autowired
-	private DepreciationService ds;
-
-	/*@Test
-	public void addGetVehiclePrice() {
-		System.out.println("Price Of Vehicle is "+ds.getPriceOfVehilce(2001));	
-	}*/
-	
-	/*@Test
-	public void addGetVehicleAge() {
-		LocalDate firstDate =LocalDate.now();
-		LocalDate secondDate = ds.getAgeOfVehicle(2002);
-		//long time_difference = date2.getTime() - date1.getTime();  
-		System.out.println(Period.between(firstDate, secondDate));
-	}*/
-	
-	/*@Test
-	public void check() {
-		System.out.println(ds.getDepreciationAmount(500000, LocalDate.of(2020, 02, 15)));
-		//System.out.println("months between"+ds.addDepreciation(depreciation)
-	}*/
-	
-	
-	
 	
 	@Test
 	public void getCustomer() {
@@ -48,6 +30,9 @@ class GeneralInsuranceApplicationTests {
 		int sc = repo.fetchByEmailAndPassword("anand@gmail.com", "123");
 		System.out.println(sc);
 	}
+	
+	
+	
 
 	
 	
