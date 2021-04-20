e9bec0349fa8728c362bf4cbdd15f46f3a6a64dd
harshit <harshitsingh0608@gmail.com>
2021-04-20 17:15:35 +0530

    create policy controller class

--- General-Insurance/src/main/java/com/lti/controller/PolicyController.java
+++ General-Insurance/src/main/java/com/lti/controller/PolicyController.java
@@ -1,6 +1,25 @@
 package com.lti.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lti.dto.VehicleStatus;
+import com.lti.entity1.Policy;
+import com.lti.entity1.Vehicle;
+import com.lti.exception.VehicleServiceException;
+import com.lti.service.PolicyService;
+
+@RestController
+@CrossOrigin
 public class PolicyController {
+	
+	@Autowired
+	private PolicyService  policyService;
+	
+	
 
 	
 	
