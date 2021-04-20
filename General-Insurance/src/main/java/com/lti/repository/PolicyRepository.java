84cadf4d6170d43d92e6f53a57346e2536dbb28a
Chathura M S <chathurams33@gmail.com>
2021-04-20 13:18:38 +0530

    Policy backend

---
+++ General-Insurance/src/main/java/com/lti/repository/PolicyRepository.java
@@ -0,0 +1,22 @@
+package com.lti.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PolicyRepository extends GenericRepository{
+	
+	public boolean isVehicleAlreadyInsured(int id) {
+		return (Long)
+				entityManager
+				.createQuery("select count(v.id) from Vehicle v where v.id= :id")
+				.setParameter("id", id)
+				.getSingleResult()== 1 ? true : false;
+	}
+	
+	public int fetchVehiclePriceAndDepreciationAmount(int vid) {
+		return (Integer)
+				entityManager
+				.createQuery("select ")
+		
+	}
+}
