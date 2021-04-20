1ff09f3b5debf3bacc65b1346fbb622b5e654e4a
Rituraj <riturajgujare01@gmail.com>
2021-04-20 15:52:10 +0530

    random changes

--- General-Insurance/src/main/java/com/lti/service/DepreciationService.java
+++ General-Insurance/src/main/java/com/lti/service/DepreciationService.java
@@ -26,6 +26,7 @@ public class DepreciationService {
 	public Depreciation addDepreciationToVehicle(int id) {
 		int price= depriciationRepo.fetchVehiclePrice(id);
 		
+		
 		LocalDate registrationDate= depriciationRepo.fetchVehicleAge(id);
 		LocalDate todayDate= LocalDate.now();
 		
